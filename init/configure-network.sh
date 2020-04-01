6480cda8920f2efe8064aea9ac0a9f7752c8bd17
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2020-04-01 23:31:19 +0100

    Configure Nginx for WPMS

--- init/configure-network.sh
+++ init/configure-network.sh
@@ -4,12 +4,6 @@
 MOJ_NETWORK_TABLE_NAME='sitemeta'
 # define the anchor
 MOJ_WP_ANCHOR="/*multisite-network*/"
-# where is wp-config
-MOJ_LOCATION_WP_CONFIG="/bedrock/web/wp-config.php"
-# where is ims-global
-MOJ_MS_GLOBAL_DIR="/bedrock/web/app/ms-global"
-# admin email address
-MOJ_S_ADMIN_USERNAME="ms-admin-team"
 # admin email address
 MOJ_DEFAULT_SITE="default-site"
 # colours
@@ -18,11 +12,11 @@ MOJ_COLOUR_END="\033[0m"
 
 # generate wp-config.php
 # check if the file exists...
-if [[ ! -f "$MOJ_LOCATION_WP_CONFIG" ]]
+if [[ ! -f "$WPMS_LOCATION_WP_CONFIG" ]]
 then
 	echo "Starting with wp-config generation."
 
-	touch ${MOJ_LOCATION_WP_CONFIG}
+	touch "${WPMS_LOCATION_WP_CONFIG}"
 
 	{
 	  printf "<?php\n"
@@ -30,7 +24,7 @@ then
     printf "require_once(dirname(__DIR__) . '/config/application.php');\n\n"
     printf "%s" "$MOJ_WP_ANCHOR"
     printf "\n\nrequire_once(ABSPATH . 'wp-settings.php');\n"
-	} >> ${MOJ_LOCATION_WP_CONFIG}
+	} >> "${WPMS_LOCATION_WP_CONFIG}"
 
 	echo "Done... configuring network..."
 fi
@@ -46,7 +40,7 @@ else
 	echo "Beginning network installation..."
 
 	wp core multisite-install --allow-root --title="MoJ Intranet" \
-	 --admin_user="${MOJ_S_ADMIN_USERNAME}" \
+	 --admin_user="${WPMS_S_ADMIN_USERNAME}" \
 	 --admin_email="${MOJ_ADMIN_EMAIL}" \
 	 --admin_password="${NETWORK_INIT_SA_PASSWORD}" \
 	 --skip-config \
@@ -155,7 +149,7 @@ else
 		# loop over directories
 		for d in */; do
 			echo ""
-			php "${MOJ_MS_GLOBAL_DIR}/import/load-site.php" "$d"
+			php "${WPMS_GLOBAL_DIR}/import/load-site.php" "$d"
 			echo -e "${MOJ_COLOUR_GREEN}Completed${MOJ_COLOUR_END}: $d"
 		done
 
@@ -167,7 +161,7 @@ else
 	MOJ_ADMIN_LOGIN_SCREEN="/wp-admin/"
 	echo -e "- - - - - - -  ${MOJ_COLOUR_GREEN}C O N G R A T U L A T I O N S${MOJ_COLOUR_END} - ${MOJ_COLOUR_GREEN}Multisite HAS BEEN INSTALLED${MOJ_COLOUR_END} - - - - - - -"
 	echo ""
-	echo "USER : $MOJ_S_ADMIN_USERNAME"
+	echo "USER : $WPMS_S_ADMIN_USERNAME"
 	echo "LOGIN: $WP_SITEURL$MOJ_ADMIN_LOGIN_SCREEN"
 	echo ""
 fi
